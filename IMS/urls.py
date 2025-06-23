5cda47590c628cff64850974c8dfbb8bfa7af3f9
Krishna Pathak <kriishna.pathak@gmail.com>
2025-06-23 07:57:04 +0545

    api testing

--- IMS/urls.py
+++ IMS/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base.views import ProductTypeApiView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/types/',
+         ProductTypeApiView.as_view({'get': 'list', 'post': 'create'})),
+    path('product/types/<int:pk>/', ProductTypeApiView.as_view(
+        {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}))
 ]
